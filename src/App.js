7d39cfca9ae18c052b70cedd9b1ac9a0eae07b79
Michael DeMory <mjdemory2891@gmail.com>
2020-10-02 12:33:43 -0400

    initial commit. have beginnings of frontend and backend connections. Need to add buttons for extra actions.

--- src/App.js
+++ src/App.js
@@ -2,25 +2,42 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts:[],
+  };
+}
+
+componentDidMount() {
+  fetch("http://127.0.0.1:8000/api/roastboast/")
+  .then(res => res.json())
+  .then(data => this.setState ({posts:data}))
+}
+
+render() {
+  return(
+    <div>
+      <ul>
+          {this.state.posts.map(p=>(
+          <div>
+            <ul>
+              <li>
+                  <h3>This is a:</h3>{p.choices = this.state.choices === true ? "Boast":"Roast"},
+                  <h3>Message:</h3>{p.body},
+                  <h3>Upvote:</h3>{p.upvote},
+                  <h3>Downvote:</h3>{p.downvote},
+                  <h3>Total Votes:</h3>{p.vote_score}
+              </li>
+            </ul>
+          </div>
+          ))}
+      </ul>
     </div>
-  );
+    )
+
+  };  
 }
 
 export default App;
