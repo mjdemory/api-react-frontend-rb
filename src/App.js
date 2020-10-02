59e25eea63452a6cffa02c78ca1778df5ae67918
Michael DeMory <mjdemory2891@gmail.com>
2020-10-02 18:30:58 -0400

    Different filter views and creating a post are working. Upvote/downvote buttons are not functional.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import New_Post from "./components/New_Post";
 
 class App extends React.Component {
   constructor(props) {
@@ -11,14 +12,43 @@ class App extends React.Component {
 }
 
 componentDidMount() {
+  this.SeeAll()
+}
+
+BoastView = () => {
+  fetch("http://127.0.0.1:8000/api/roastboast/BoastViewSet/")
+  .then(res => res.json())
+  .then(data => this.setState ({posts:data}))
+}
+
+RoastView = () => {
+  fetch("http://127.0.0.1:8000/api/roastboast/RoastViewSet/")
+  .then(res => res.json())
+  .then(data => this.setState ({posts:data}))
+
+}
+
+MostPopularView = () => {
+  fetch("http://127.0.0.1:8000/api/roastboast/VoteScoreViewSet/")
+  .then(res => res.json())
+  .then(data => this.setState ({posts:data}))
+}
+
+SeeAll = () => {
   fetch("http://127.0.0.1:8000/api/roastboast/")
   .then(res => res.json())
   .then(data => this.setState ({posts:data}))
+
 }
 
 render() {
   return(
     <div>
+      <New_Post/>
+      <button onClick={this.SeeAll}>See All</button>
+      <button onClick={this.BoastView}>View Boasts</button>
+      <button onClick={this.RoastView}>View Roasts</button>
+      <button onClick={this.MostPopularView}>Most Popular Posts</button>
       <ul>
           {this.state.posts.map(p=>(
           <div>
